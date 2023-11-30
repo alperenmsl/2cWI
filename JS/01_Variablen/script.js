32fd1fa533b11aa0fa5f5edd9a3749e35bc5f239
Alperen Meseli <alperen.meseli@student.htldornbirn.at>
2023-11-30 08:33:07 +0100

    02 strings

---
+++ JS/01_Variablen/script.js
@@ -0,0 +1,41 @@
+console.log("Hallo");
+console.log("Tolle Sache");
+// Variablen
+let a = 7;
+let b = 8;
+let c = 10;
+// Berechne folgendes und gib das Eregebnis über console.log aus
+// a + b + c
+// (a + b) / c
+// c -a + b
+// a + b + c * 3
+// (a + b + c)/4
+
+console.log(a + b + c);
+console.log((a + b) /c );
+console.log(c - a + b);
+console.log( a + b + c * 3);
+console.log((a + b + c)/4);
+
+console.log("Neue Variablen");
+
+let a2 = 3;
+let b2 = a + 3;
+let c2 = c - 3;
+// Ändere die Variablen auf folgende Werte
+// a = 3
+// b = a + 3
+// c = c -3
+
+// a + b + c
+// (a + b) / c
+// c -a + b
+// a + b + c * 3
+// (a + b + c)/4
+
+console.log(a2 + b2 + c2);
+console.log((a2 + b2) / c2);
+console.log(c2 - a2 + b2);
+console.log(a2 + b2 + c2 * 3);
+console.log((a2 + b2 + c2)/4);
+// gib die neuen Werte der Variablen aus
