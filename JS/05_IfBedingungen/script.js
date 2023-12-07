2f06502e1f6982b575437d0bc076e323343f66a2
Alperen Meseli <alperen.meseli@student.htldornbirn.at>
2023-12-07 08:53:04 +0100

    IfBedingungen05 Finished

---
+++ JS/05_IfBedingungen/script.js
@@ -0,0 +1,49 @@
+// erstelle eine Variable number
+let a = 8
+
+// Aufgabe 1
+// Wenn die Zahl kleiner ist als 20  gib aus "Mini"
+if (a < 20) {
+    console.log("Mini");
+}
+// Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
+
+a = 30
+
+if (20 <= a  && a <=50 ){
+    console.log("Medium");
+}
+
+// Wenn die Zahl größer als 50 ist gib aus "Large"
+
+a = 55
+
+if (50 < a) {
+    console.log("Large");
+}
+
+
+// Aufgabe 2
+// Wenn die Zahl kleiner gleich 7 ist gib aus "Mini"
+
+a = 7
+
+if (a <= 7){
+    console.log("Mini");
+}
+
+// Wenn die Zahl zwischen 7 und 90 ist gib  aus "Medium"
+
+a = 19
+
+if (7 < a && a < 90){
+    console.log("Medium");
+}
+
+// Wenn die Zahl größer als 90 ist gib aus "Large"
+
+a = 131
+
+if (a > 90){
+    console.log("Large");
+}
