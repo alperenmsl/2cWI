dbb096752fa7930f5106ccf6b48f9559ceea9f00
unknown <alperen.meseli@student.htldornbirn.at>
2024-01-11 09:22:14 +0100

    test

---
+++ JS/09_AufgabeModulo/script.js
@@ -0,0 +1,9 @@
+let summe = 0;
+
+for (let zahl = 2; zahl <= 100; zahl += 2) {
+    summe += zahl;
+    console.log(zahl);
+}
+
+console.log("Die Summe aller geraden Zahlen von 2 bis 100 ist:", summe);
+
