d7c16c18bda0d03c4d768b4da01a58a33e0e9ec3
unknown <alperen.meseli@student.htldornbirn.at>
2024-03-21 08:58:41 +0100

    BankomatenBeispiel

--- JS/13_BankomatenBeispiel/selfmadescript.js
+++ JS/13_BankomatenBeispiel/selfmadescript.js
@@ -1,50 +1,47 @@
 
 import { createInterface } from "readline";
+ 
 const readline = createInterface({
-    input: process.stdin,
-    output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
-const readlineAsync = () => {
-    return new Promise((resolve) => {
-        readline.question("", (userRes) => {
-            resolve(userRes)
-        });
+ 
+const readLineAsync = () => {
+  return new Promise((resolve) => {
+    readline.question("", (userRes) => {
+      resolve(userRes);
+ 
     });
+  });
 };
-
-let balance = 0;
-
-
-while(true){
-    console.log("Bitte selektieren sie");
-    let selection = await readlineAsync();
-    console.log("Du hast gewählt " + selection);
+ 
+let kontoStand = 0;
+let isFinished = false;
+ 
+ 
+ 
+while(!isFinished){
+console.log("1. Einzahlen");
+console.log("2. Abheben");
+console.log("3. Konstostand");
+console.log("4. Beeden");
+let selection = await readLineAsync();
+if(selection == 1){
+    console.log("Wie viel € möchten Sie einzahlen");
+   let einzahlung =  Number(await readLineAsync());
+   kontoStand = kontoStand + einzahlung;
+   console.log("Ihre Einzahlung würde erfolgreich abgeschlossen");
 }
-
-const einzahlung = (amount) => {
-    balance+= amount
-    console.log(`Sie haben erfolgreich einen betrag von ${amount}€ eingezahlt`);
-};
-
-const abhebung = (amount) => {
-    if (balance>= amount){
-        balance-= amount
-        console.log(`Sie haben erfolgreich einen betrag von ${amount}€ abgehoben`);
-    } else if(balance <= amount){
-        console.log(`Sie haben nicht genügend Guthaben auf dem Konto`);
-    }
-};
-
-const kontostand = (amount) => {
-    console.log(`Sie haben aktuell ${balance}€ auf ihrem Konto`);
-};
-
-const beenden = () =>{
-    return 0
-};
-
-
-
-
-
-readline.close();
+else if(selection == 2){
+    console.log("Wie viel € möchten Sie abheben");
+   let abhebung =  Number(await readLineAsync());
+   kontoStand = kontoStand - abhebung;
+   console.log("Ihre Abhebung würde erfolgreich abgeschlossen");
+}
+else if(selection == 3){
+    console.log("Ihre Kontostand: " + kontoStand);
+}
+else if(selection == 4){
+    readline.close();
+}
+}
